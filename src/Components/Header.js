1421431f9bfbd4f28173dd8098a94b811703a9ed
Muahmmad <muhammadmagdyje@gmail.com>
2020-10-01 04:11:42 +0200

    initialization

---
+++ src/Components/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {AntDesign,Octicons,Ionicons,MaterialCommunityIcons} from '@expo/vector-icons';
+import Constant from 'expo-constants'
+
+export default function Header (){
+    return (
+        <View style = {{
+            marginTop:Constant.statusBarHeight,
+            height:40,
+            backgroundColor:"white",
+            flexDirection:'row',
+            justifyContent:'space-between',
+            elevation:1
+        }}>
+            <View style = {{
+                flexDirection:'row',
+                margin:5
+                }}>
+                <AntDesign 
+                    name = "youtube" 
+                    size = {32} 
+                    color = "red"
+                    style = {{
+                        marginLeft:20
+                    }}
+                />
+                <Text style = {{
+                    fontSize:20,
+                    marginLeft:5,
+                    fontWeight:'bold'
+                }} > Youtube </Text>
+            </View>
+            <View style = {{
+                flexDirection : 'row',
+                justifyContent:'space-around',
+                width:150,
+                margin:7
+            }}>
+                <Octicons name="device-camera-video" size={24} color="#c7c5c1" />
+                <Ionicons name="ios-search" size={24} color="#c7c5c1" />
+                <MaterialCommunityIcons name="account-circle" size={24} color="#c7c5c1" />
+            </View>
+        </View>
+    );
+}
---
+++ src/Screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Header from '../Components/Header.js';
+
+export default function Home () {
+    return (
+        <View>
+            <Header/>
+            <Text>Home page</Text>
+        </View>
+    );
+}

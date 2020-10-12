de1e5e6a3aed2d884c8c81fca1125a55a43a8f9f
Muhammad magdy <58988660+muhammadmb@users.noreply.github.com>
2020-10-12 23:43:58 +0200

    make video player component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import {Image, View, Text, Dimensions} from 'react-native';
+import {Image, View, Text, Dimensions,TouchableOpacity} from 'react-native';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native'
+
 const Card = (props) =>{
   return(
-       <View style = {{marginBottom:6}}>
-        <Image
-            source = {{uri:`https://i.ytimg.com/vi/${props.video}/default.jpg`}}
-            style = {{
-                height:200,
-            }}
-        />
-        <View style={{
-            flexDirection:'row',
-            margin:5
-            }}
-        >
-            <MaterialCommunityIcons name="account-circle" size={44} color="black" />
-            <View style = {{justifyContent :'space-evenly'}}>
-                <Text style={{fontWeight:'bold', width:Dimensions.get("screen").width - 60}} ellipsizeMode = "tail" numberOfLines={2}>
-                    {props.title} 
-                </Text>
-                <Text style = {{fontSize:12,color:'#c7c5c1'}} >{props.channel} . 14k views . 7 houres ago</Text>
-            </View>
+      <TouchableOpacity
+        onPress = {() => console.log(props)}
+      >
+        <View style = {{marginBottom:6}}>
+            <Image
+                source = {{uri:`https://i.ytimg.com/vi/${props.video}/default.jpg`}}
+                style = {{
+                    height:200,
+                }}
+            />
+            <View style={{
+                flexDirection:'row',
+                margin:5
+                }}
+            >
+                <MaterialCommunityIcons name="account-circle" size={44} color="black" />
+                <View style = {{justifyContent :'space-evenly'}}>
+                    <Text style={{fontWeight:'bold', width:Dimensions.get("screen").width - 60}} ellipsizeMode = "tail" numberOfLines={2}>
+                        {props.title} 
+                    </Text>
+                    <Text style = {{fontSize:12,color:'#c7c5c1'}} >{props.channel} . 14k views . 7 houres ago</Text>
+                </View>
 
+            </View>
         </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 export default Card;
--- src/Screens/VideoPlayer.js
+++ src/Screens/VideoPlayer.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import {View,Text, FlatList,ActivityIndicator} from 'react-native';
+import {View,Text, FlatList,Dimensions} from 'react-native';
 import Header from '../Components/Header.js';
+import { WebView } from 'react-native-webview';
+import Constant from 'expo-constants'
 
-const VideoPlayer = () =>{
+const VideoPlayer = (props) =>{
     return (
-        <View style ={{flex:1}} >
+        <View style ={{flex:1, marginTop:Constant.statusBarHeight}} >
             <Header/>
-            <Text>
-                VideoPlayer Screen
+            <View style = {{
+                width:"100%",
+                height:200
+            }} >
+                <Webview
+                    source = {{uri:`https://www.youtube.com/embed/${props.videoId}`}}
+                />
+            </View>
+            <Text style = {{
+                fontSize:20,
+                width:Dimensions.get("screen").width - 50,
+                margin:9
+            }}
+            numberOfLines = {2}
+            >
+                {props.title}
             </Text>
+            <View 
+                style = {{borderBottomWidth:1}}
+            />
         </View>
     );
 }
